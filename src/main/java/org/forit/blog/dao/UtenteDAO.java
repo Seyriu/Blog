5c74fb6a3a9ff1d7898216368dd4e7ef9382a8c0
DESKTOP-VKNOI85\Casper <denis.coccodi@gmail.com>
2018-06-11 18:57:28 +0200

    login error counter, user activation and ban system implemented

--- src/main/java/org/forit/blog/dao/UtenteDAO.java
+++ src/main/java/org/forit/blog/dao/UtenteDAO.java
@@ -26,140 +26,205 @@ import org.forit.blog.exceptions.BlogException;
  */
 public class UtenteDAO {
 
-  public UtenteDTO UtenteEntityToUtenteDTO(UtenteEntity uEntity) {
-    RuoloDTO rDTO = null;
+    public UtenteDTO UtenteEntityToUtenteDTO(UtenteEntity uEntity) {
+        RuoloDTO rDTO = null;
 
-    if (uEntity.getRuolo() != null) {
-      rDTO = new RuoloDTO(
-              uEntity.getRuolo().getId(),
-              uEntity.getRuolo().getNome()
-      );
+        if (uEntity.getRuolo() != null) {
+            rDTO = new RuoloDTO(
+                    uEntity.getRuolo().getId(),
+                    uEntity.getRuolo().getNome()
+            );
+        }
+
+        UtenteDTO uDTO = new UtenteDTO(
+                uEntity.getId(),
+                uEntity.getEmail(),
+                uEntity.getIsActive(),
+                uEntity.getFailed_access_attempts(),
+                uEntity.getIsBanned(),
+                uEntity.getDateCreation(),
+                uEntity.getDateLastAccess(),
+                rDTO);
+
+        return uDTO;
     }
 
-    UtenteDTO uDTO = new UtenteDTO(
-            uEntity.getId(),
-            uEntity.getEmail(),
-            uEntity.getIsActive(),
-            uEntity.getFailed_access_attempts(),
-            uEntity.getIsBanned(),
-            uEntity.getDateCreation(),
-            uEntity.getDateLastAccess(),
-            rDTO);
+    public UtenteEntity utenteDTOToUtenteEntity(UtenteDTO uDTO) {
+        RuoloEntity rEntity = null;
 
-    return uDTO;
-  }public UtenteEntity utenteDTOToUtenteEntity(UtenteDTO uDTO) {
-    RuoloEntity rEntity = null;
+        if (uDTO.getRuolo() != null) {
+            rEntity = new RuoloEntity(
+                    uDTO.getRuolo().getId(),
+                    uDTO.getRuolo().getNome()
+            );
+        }
 
-    if (uDTO.getRuolo() != null) {
-      rEntity = new RuoloEntity(
-              uDTO.getRuolo().getId(),
-              uDTO.getRuolo().getNome()
-      );
+        UtenteEntity uEntity = new UtenteEntity(
+                uDTO.getId(),
+                uDTO.getEmail(),
+                uDTO.getPassword(),
+                uDTO.getIsActive(),
+                uDTO.getFailedAccessAttempts(),
+                uDTO.getIsBanned(),
+                uDTO.getDateCreation(),
+                uDTO.getDateLastAccess(),
+                rEntity);
+
+        return uEntity;
     }
 
-    UtenteEntity uEntity = new UtenteEntity(
-            uDTO.getId(),
-            uDTO.getEmail(),
-            uDTO.getPassword(),
-            uDTO.getIsActive(),
-            uDTO.getFailedAccessAttempts(),
-            uDTO.getIsBanned(),
-            uDTO.getDateCreation(),
-            uDTO.getDateLastAccess(),
-            rEntity);
+    public List<UtenteDTO> getListaUtenti() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<UtenteEntity> query = em.createNamedQuery("utente.selectAll", UtenteEntity.class);
+        List<UtenteEntity> list = query.getResultList();
+        List<UtenteDTO> uDTO = list.stream().map(uEntity -> {
+            UtenteDAO uDAO = new UtenteDAO();
+            return uDAO.UtenteEntityToUtenteDTO(uEntity);
+        }).collect(Collectors.toList());
+        em.close();
+        emf.close();
 
-    return uEntity;
-  }
-
-  public List<UtenteDTO> getListaUtenti() {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
-    EntityManager em = emf.createEntityManager();
-    TypedQuery<UtenteEntity> query = em.createNamedQuery("utente.selectAll", UtenteEntity.class);
-    List<UtenteEntity> list = query.getResultList();
-    List<UtenteDTO> uDTO = list.stream().map(uEntity -> {
-      UtenteDAO uDAO = new UtenteDAO();
-      return uDAO.UtenteEntityToUtenteDTO(uEntity);
-    }).collect(Collectors.toList());
-    em.close();
-    emf.close();
-
-    return uDTO;
-  }
-
-  public UtenteDTO loadUtente(long id) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
-    EntityManager em = emf.createEntityManager();
-    UtenteEntity utenteEntity = em.find(UtenteEntity.class, id);
-    UtenteDTO uDTO = this.UtenteEntityToUtenteDTO(utenteEntity);
-    em.close();
-    emf.close();
-
-    return uDTO;
-  }
-
-  public UtenteEntity loadUtenteByEmail(String email) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
-    EntityManager em = emf.createEntityManager();
-    TypedQuery<UtenteEntity> query = em.createNamedQuery("utente.selectUserByEmail", UtenteEntity.class)
-            .setParameter("email", email);
-    UtenteEntity uEntity = query.getResultList().get(0);
-    em.close();
-    emf.close();
-
-    return uEntity;
-  }
-
-  public void insertUtente(UtenteDTO uDTO) throws BlogException {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu");
-    EntityManager em = emf.createEntityManager();
-
-    EntityTransaction transaction = em.getTransaction();
-    try {
-      transaction.begin();
-
-      UtenteEntity uEntity = new UtenteEntity();
-      uEntity.setEmail(uDTO.getEmail());
-      uEntity.setPassword(uDTO.getPassword());
-      uEntity.setDateCreation(uDTO.getDateCreation());
-      uEntity.setDateLastAccess(uDTO.getDateLastAccess());
-      uEntity.setFailed_access_attempts(0);
-      uEntity.setIsActive(uDTO.getIsActive());
-      uEntity.setIsBanned(uDTO.getIsBanned());
-      if (uDTO.getRuolo() != null) {
-        uEntity.setRuolo(new RuoloEntity(
-                uDTO.getRuolo().getId(),
-                uDTO.getRuolo().getNome()
-        ));
-      } else {
-        uEntity.setRuolo(null);
-      }
-
-      em.persist(uEntity);
-      transaction.commit();
-    } catch (Exception ex) {
-      transaction.rollback();
-      throw new BlogException(ex);
-    } finally {
-      em.close();
-      emf.close();
+        return uDTO;
     }
-  }
 
-  public List<String> login(String path, String email, String password) {
-    try {
-        List<String> jwt = new ArrayList<>();
-      UtenteEntity uEntity = loadUtenteByEmail(email);
-      UtenteDTO uDTO = this.UtenteEntityToUtenteDTO(uEntity);
-      uDTO.setPassword(uEntity.getPassword());
-      if (password.equals(uDTO.getPassword())) {
-        Authentication auth = new Authentication();
-        jwt.add(auth.getJWS(path, uDTO.getEmail(), uDTO.getRuolo().getNome()));
-        jwt.add(Long.toString(uEntity.getId()));
-        return jwt;
-      }
-      return null;
-    } catch (BlogException ex) {
-      return null;
+    public UtenteDTO loadUtente(long id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+        UtenteEntity utenteEntity = em.find(UtenteEntity.class, id);
+        UtenteDTO uDTO = this.UtenteEntityToUtenteDTO(utenteEntity);
+        em.close();
+        emf.close();
+
+        return uDTO;
     }
-  }
+
+    public UtenteEntity loadUtenteByEmail(String email) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<UtenteEntity> query = em.createNamedQuery("utente.selectUserByEmail", UtenteEntity.class)
+                .setParameter("email", email);
+        UtenteEntity uEntity = query.getResultList().get(0);
+        em.close();
+        emf.close();
+
+        return uEntity;
+    }
+
+    public void insertUtente(UtenteDTO uDTO) throws BlogException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            UtenteEntity uEntity = new UtenteEntity();
+            uEntity.setEmail(uDTO.getEmail());
+            uEntity.setPassword(uDTO.getPassword());
+            uEntity.setDateCreation(uDTO.getDateCreation());
+            uEntity.setDateLastAccess(uDTO.getDateLastAccess());
+            uEntity.setFailed_access_attempts(0);
+            uEntity.setIsActive(uDTO.getIsActive());
+            uEntity.setIsBanned(uDTO.getIsBanned());
+            if (uDTO.getRuolo() != null) {
+                uEntity.setRuolo(new RuoloEntity(
+                        uDTO.getRuolo().getId(),
+                        uDTO.getRuolo().getNome()
+                ));
+            } else {
+                uEntity.setRuolo(null);
+            }
+
+            em.persist(uEntity);
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new BlogException(ex);
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+    public void updateActivated(boolean isActive, long id) throws BlogException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            UtenteEntity utente = em.find(UtenteEntity.class, id);
+            utente.setIsActive(isActive);
+            em.merge(utente);
+
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new BlogException(ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void updateBanned(boolean isBanned, long id) throws BlogException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            UtenteEntity utente = em.find(UtenteEntity.class, id);
+            utente.setIsBanned(isBanned);
+            em.merge(utente);
+
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new BlogException(ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void increaseFailedAccessAttempts(String email) throws BlogException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog_pu"); // nome dato in persistence.xml
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            UtenteEntity utente = this.loadUtenteByEmail(email);
+            utente.setFailed_access_attempts(utente.getFailed_access_attempts() + 1);
+            em.merge(utente);
+
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new BlogException(ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<String> login(String path, String email, String password) {
+        try {
+            List<String> jwt = new ArrayList<>();
+            UtenteEntity uEntity = loadUtenteByEmail(email);
+            UtenteDTO uDTO = this.UtenteEntityToUtenteDTO(uEntity);
+            uDTO.setPassword(uEntity.getPassword());
+            if (password.equals(uDTO.getPassword())) {
+                Authentication auth = new Authentication();
+                jwt.add(auth.getJWS(path, uDTO.getEmail(), uDTO.getRuolo().getNome()));
+                jwt.add(Long.toString(uEntity.getId()));
+                return jwt;
+            }
+            return null;
+        } catch (BlogException ex) {
+            return null;
+        }
+    }
 }
--- src/main/java/org/forit/blog/rest/UtenteRest.java
+++ src/main/java/org/forit/blog/rest/UtenteRest.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,45 +26,118 @@ import org.forit.blog.exceptions.BlogException;
 @Path("/utenti")
 public class UtenteRest {
 
-  @Path("/")
-  @GET
-  @Produces("application/json")
-  public List<UtenteDTO> loadUtenti() {
-    UtenteDAO uDAO = new UtenteDAO();
-    return uDAO.getListaUtenti();
-  }
-
-  @Path("/{id}")
-  @GET
-  @Consumes("application/json")
-  @Produces("application/json")
-  public UtenteDTO loadUtente(@PathParam("id") Long id, @HeaderParam("jwt") String compactJwt) {
-    try {
-      Authentication auth = new Authentication();
-      if (auth.checkJWSUtenteOrAdmin(compactJwt)) {
-        UtenteDAO uDAO = new UtenteDAO();
-        return uDAO.loadUtente(id);
-      } else {
-        return null;
-      }
-    } catch (BlogException ex) {
-      System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
-      return null;
+    @Path("/")
+    @GET
+    @Produces("application/json")
+    public List<UtenteDTO> loadUtenti(@HeaderParam("jwt") String compactJwt) {
+        try {
+            Authentication auth = new Authentication();
+            if (auth.checkJWSAdmin(compactJwt)) {
+                UtenteDAO uDAO = new UtenteDAO();
+                return uDAO.getListaUtenti();
+            }
+            return null;
+        } catch (BlogException ex) {
+            return null;
+        }
     }
-  }
 
-  @Path("/")
-  @POST
-  @Consumes("application/json")
-  @Produces("application/json")
-  public boolean insertUtente(UtenteDTO uDTO) {
-    try{
-        UtenteDAO uDAO = new UtenteDAO();
-        uDAO.insertUtente(uDTO);
-        return true;
-    } catch (BlogException ex) {
-      System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
-      return false;
+    @Path("/{id}")
+    @GET
+    @Consumes("application/json")
+    @Produces("application/json")
+    public UtenteDTO loadUtente(@PathParam("id") Long id,
+            @HeaderParam("jwt") String compactJwt
+    ) {
+        try {
+            Authentication auth = new Authentication();
+            if (auth.checkJWSUtenteOrAdmin(compactJwt)) {
+                UtenteDAO uDAO = new UtenteDAO();
+                return uDAO.loadUtente(id);
+            } else {
+                return null;
+            }
+        } catch (BlogException ex) {
+            System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
+            return null;
+        }
     }
-  }
+
+    @Path("/")
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public boolean insertUtente(UtenteDTO uDTO
+    ) {
+        try {
+            UtenteDAO uDAO = new UtenteDAO();
+            uDAO.insertUtente(uDTO);
+            return true;
+        } catch (BlogException ex) {
+            System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    @Path("/activated/{id}")
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public boolean updateActivated(boolean isActive,
+            @PathParam("id") long id,
+            @HeaderParam("jwt") String compactJwt
+    ) {
+        try {
+            Authentication auth = new Authentication();
+            if (auth.checkJWSAdmin(compactJwt)) {
+                UtenteDAO uDAO = new UtenteDAO();
+                uDAO.updateActivated(isActive, id);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (BlogException ex) {
+            System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    @Path("/banned/{id}")
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public boolean updateBanned(boolean isBanned,
+            @PathParam("id") long id,
+            @HeaderParam("jwt") String compactJwt
+    ) {
+        try {
+            Authentication auth = new Authentication();
+            if (auth.checkJWSAdmin(compactJwt)) {
+                UtenteDAO uDAO = new UtenteDAO();
+                uDAO.updateBanned(isBanned, id);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (BlogException ex) {
+            System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    @Path("/failed-accesses")
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public boolean increaseFailedAccessAttempts(String email
+    ) {
+        try {
+            UtenteDAO uDAO = new UtenteDAO();
+            uDAO.increaseFailedAccessAttempts(email);
+            return true;
+        } catch (BlogException ex) {
+            System.out.println("Si e' verificato un errore: " + ex.getLocalizedMessage());
+            return false;
+        }
+    }
 }
